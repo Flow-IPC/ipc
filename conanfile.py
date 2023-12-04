7f0433364ae5a19c6106c1aa1cdc9317f34d2285
kinokrt <43879397+kinokrt@users.noreply.github.com>
2023-12-04 22:12:17 +0100

    Update conanfile.py
    Separate DOC and BUILD target.

--- conanfile.py
+++ conanfile.py
@@ -2,26 +2,43 @@ from conan import ConanFile
 from conan.tools.cmake import cmake_layout, CMakeDeps
 
 class IpcRecipe(ConanFile):
+    name = "ipc"
+    
     settings = "os", "compiler", "build_type", "arch"
+    
     generators = (
         "CMakeToolchain"
     )
 
-    requires = (
-        "flow/1.0",
-        "gtest/1.14.0",
-        "jemalloc/5.2.1"
-    )
-    
     tool_requires = (
         "cmake/3.26.3", 
-        "doxygen/1.9.4"
     )
+
+    options = {
+        "build": [True, False], 
+        "doc": [True, False]
+    }
     
+    default_options = {
+        "build": True, 
+        "doc": False
+    }
+
+    def requirements(self):
+        if self.options.build:
+            self.requires("flow/1.0")
+            self.requires("gtest/1.14.0")
+            self.requires("jemalloc/5.2.1")
+    
+    def build_requirements(self):
+        if self.options.doc:
+            self.tool_requires("doxygen/1.9.4")
+            
     def layout(self):
         cmake_layout(self)
 
     def generate(self):
         cmake = CMakeDeps(self)
-        cmake.build_context_activated = ["doxygen/1.9.4"]
+        if self.options.doc:
+            cmake.build_context_activated = ["doxygen/1.9.4"]
         cmake.generate()
