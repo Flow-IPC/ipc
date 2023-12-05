101ceeea706e24ca5a4c1c33aaf7d0590dbda392
Yuri Goldfeld <yuri.goldfeld@yahoo.com>
2023-12-04 18:40:32 -0800

    Flow should not need capnp; that is an ipc* tequirement; moving that dep from flow/conanfile.py to ipc/conanfile.py.

--- conanfile.py
+++ conanfile.py
@@ -26,6 +26,7 @@ class IpcRecipe(ConanFile):
 
     def requirements(self):
         if self.options.build:
+            self.requires("capnproto/1.0.1")
             self.requires("flow/1.0")
             self.requires("gtest/1.14.0")
             self.requires("jemalloc/5.2.1")
