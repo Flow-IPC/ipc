eb4e910b75f05cd3040b6cf10114192de7a9dc37
Yuri Goldfeld <yuri.goldfeld@yahoo.com>
2023-12-15 15:28:44 -0800

    Per preceding TODO -- parameterizing Conan profile to optionally forego LTO.  This may not be the most elegant way in terms of Python, but the essence is fine.

--- conanfile.py
+++ conanfile.py
@@ -7,35 +7,39 @@ class IpcRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
 
     options = {
-        "build": [True, False], 
+        "build": [True, False],
+        "build_no_lto": [True, False],
         "doc": [True, False],
     }
-    
+
     default_options = {
-        "build": True, 
+        "build": True,
+        "build_no_lto": False,
         "doc": False,
     }
 
     def configure(self):
         if self.options.build:
-            self.options["jemalloc"].enable_cxx = False 
+            self.options["jemalloc"].enable_cxx = False
             self.options["jemalloc"].prefix = "je_"
-    
+
     def generate(self):
         deps = CMakeDeps(self)
         if self.options.doc:
             deps.build_context_activated = ["doxygen/1.9.4"]
         deps.generate()
-       
+
         toolchain = CMakeToolchain(self)
         if self.options.build:
             toolchain.variables["CFG_ENABLE_TEST_SUITE"] = "ON"
             toolchain.variables["JEMALLOC_PREFIX"] = self.options["jemalloc"].prefix
+            if self.options.build_no_lto:
+                toolchain.variables["CFG_NO_LTO"] = "ON"
         if self.options.doc:
             toolchain.variables["CFG_ENABLE_DOC_GEN"] = "ON"
             toolchain.variables["CFG_SKIP_CODE_GEN"] = "ON"
         toolchain.generate()
-    
+
     def build(self):
         cmake = CMake(self)
         cmake.configure()
@@ -44,15 +48,17 @@ class IpcRecipe(ConanFile):
         if self.options.build:
             self.run("cmake --build . -- --keep-going VERBOSE=1")
         if self.options.doc:
+            # Note: `flow_doc_public flow_doc_full` could also be added here and work; however
+            # we leave that to `flow` and its own Conan setup.
             self.run("cmake --build . -- ipc_doc_public ipc_doc_full --keep-going VERBOSE=1")
-    
+
     def requirements(self):
         if self.options.build:
             self.requires("capnproto/1.0.1")
             self.requires("flow/1.0")
             self.requires("gtest/1.14.0")
             self.requires("jemalloc/5.2.1")
-    
+
     def build_requirements(self):
         self.tool_requires("cmake/3.26.3")
         if self.options.doc:
@@ -61,6 +67,6 @@ class IpcRecipe(ConanFile):
     def package(self):
         cmake = CMake(self)
         cmake.install()
-            
+
     def layout(self):
         cmake_layout(self)
