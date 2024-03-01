43b88972def47d3f7fe6908eef1aa646a6d417f2
Yuri Goldfeld <yuri.goldfeld@yahoo.com>
2024-03-01 00:52:15 -0800

    (WIP)

--- test/suite/perf_demo/main_cli.cpp
+++ test/suite/perf_demo/main_cli.cpp
@@ -150,7 +150,7 @@ void run_capnp_over_raw(flow::log::Logger* logger_ptr, Channel_raw* chan_ptr)
       // Send a dummy message as a request signal, so we can start timing RTT before sending it.
       FLOW_LOG_INFO("= Got handshake SYN.");
 
-      m_timer.emplace(get_logger(), "capnp-raw", Timer::real_clock_types());
+      m_timer.emplace(get_logger(), string("capnp-raw"), Timer::real_clock_types());
       FLOW_LOG_INFO("> Issuing get-cache request via tiny message.");
       m_chan.send_blob(Blob_const(&m_n, sizeof(m_n)));
       m_timer->checkpoint("sent request");
