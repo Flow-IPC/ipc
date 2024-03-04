b6c8f641c9fa7f3fb327b449aad6b3c670f5ee6e
Yuri Goldfeld <yuri.goldfeld@yahoo.com>
2024-03-04 00:08:12 -0800

    Fixing minor test/demo logging problem.

--- test/suite/perf_demo/main_srv.cpp
+++ test/suite/perf_demo/main_srv.cpp
@@ -98,11 +98,11 @@ int main(int argc, char const * const * argv)
   // Set up logging.
   Config std_log_config;
   std_log_config.init_component_to_union_idx_mapping<Flow_log_component>(1000, 999);
-  std_log_config.init_component_names<Flow_log_component>(flow::S_FLOW_LOG_COMPONENT_NAME_MAP, false, "perf_demo-");
+  std_log_config.init_component_names<Flow_log_component>(flow::S_FLOW_LOG_COMPONENT_NAME_MAP, false, "flow-");
   std_log_config.init_component_to_union_idx_mapping<ipc::Log_component>(2000, 999);
-  std_log_config.init_component_names<ipc::Log_component>(ipc::S_IPC_LOG_COMPONENT_NAME_MAP, false, "perf_demo-");
+  std_log_config.init_component_names<ipc::Log_component>(ipc::S_IPC_LOG_COMPONENT_NAME_MAP, false, "ipc-");
   Simple_ostream_logger std_logger(&std_log_config);
-  FLOW_LOG_SET_CONTEXT(&std_logger, Flow_log_component::S_UNCAT);
+  FLOW_LOG_SET_CONTEXT(&std_logger, ipc::Log_component::S_UNCAT);
   FLOW_LOG_INFO("FYI -- Usage: " << argv[0] << " [msg payload size in MiB] [log file]");
   FLOW_LOG_INFO("FYI -- Defaults: " << TOTAL_SZ_MI << " " << LOG_FILE);
   // This is separate: the IPC/Flow logging will go into this file.
